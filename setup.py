6681758d2bbb3de01fe2ac1e55bdeafb1de670ce
kylemede <kylemede@gmail.com>
2016-12-10 17:08:38 +0900

    nc

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ else:
 
 setup(    
     name='ExoSOFT', 
-    version="1.0.66", 
+    version="1.1.1", 
     author='Kyle Mede',
     author_email = 'kylemede@gmail.com',
     url = 'https://github.com/kylemede/ExoSOFT',
