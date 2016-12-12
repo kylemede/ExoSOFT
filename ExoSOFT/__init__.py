8bf28b53d14b495661711525070cb4c83ddef70d
kylemede <kylemede@gmail.com>
2016-12-12 14:47:39 +0900

    nc

--- ExoSOFT/__init__.py
+++ ExoSOFT/__init__.py
@@ -1,7 +1,2 @@
-from ExoSOFT import exoSOFT
-from customPost import custom_post
-
-#from . import tools
-#import ExoSOFT
-#import exosoftpath
-#import simulator
+from .ExoSOFT import exoSOFT
+from .customPost import custom_post
