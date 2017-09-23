4a583e7e80a98dcd2e17fedcdf93fddd9d2ae694
kyle <kyle@localhost.localdomain>
2017-09-24 00:25:24 +0900

    incremented version number

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ else:
 
 setup(
     name='ExoSOFT',
-    version="0.3.2", 
+    version="0.3.3", 
     author='Kyle Mede',
     author_email = 'kylemede@gmail.com',
     url = 'https://github.com/kylemede/ExoSOFT',
